c21d74c91b2e391bc5203f55b5ec363278e195b3
Justice Smith <59818423+jcode94@users.noreply.github.com>
2021-03-27 17:12:32 -0400

    started C++ and uploaded prompts for A1-A5

---
+++ C++/helloworld2.cpp
@@ -0,0 +1,19 @@
+/* my second program in C++
+   with more comments */
+   
+#include <iostream>
+
+int main ()
+{
+	std::cout << "Hello World! ";		// prints Hello World!
+	std::cout << "I'm a C++ program";	// prints I'm a C++ program
+}
+/*
+using namespace std;
+
+int main ()
+{
+	cout << "Hello World!";
+	cout << "I'm a C++ program!";
+}
+*/
